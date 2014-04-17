f5bc620def184654acd82341bf045d7afc2eda94
Ben Meier <benmeier42@gmail.com>
2014-04-17 12:16:29 +0200

    added test level spawner

--- unity_project/Assets/spawn_floor.cs
+++ unity_project/Assets/spawn_floor.cs
@@ -1,22 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class spawn_floor : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject ft;
+	public TextAsset ta;
+	public float tilesize = 1.0f;
+	public float tileyoffset = -0.5f;
 
 	void Start () {
-			
-		Quaternion q = Quaternion.LookRotation(new Vector3(0.0f,1.0f,0.0f));
 
-		for(int i=0;i<16;i++) {
-			for (int j = 0;j<16;j++) {
-				bool b = Random.value > 0.3f;
-				if(b)Instantiate(ft, new Vector3(i-8,-0.5f,j-8), q);
+		var lines = ta.text.Split('\n').Select(l => l.Trim());
+
+		// calculate max line length
+		int longestline = lines.Select(l => l.Length).Max();
+		int numlines = lines.Count();
+
+		// calculate center
+		float centerx = longestline / 2.0f;
+		float centerz = numlines / 2.0f;
+
+		// tile orientation
+		Quaternion uprotation = Quaternion.LookRotation(Vector3.up);
+
+		float z = -centerz * tilesize;
+		foreach(string line in lines) {
+			float x = -centerx * tilesize;
+			foreach(char c in line) {
+				if(c == '#') {
+					Instantiate(ft, new Vector3(x, tileyoffset, z), uprotation);
+				}
+				x += tilesize;
 			}
+			z += tilesize;
 		}
-
 
 	}
 	
