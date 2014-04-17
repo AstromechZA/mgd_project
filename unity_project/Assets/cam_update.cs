530e2a7110266d6bb5c65055fec76c9b5272c627
Ben Meier <benmeier42@gmail.com>
2014-04-17 11:44:34 +0200

    added touch and keyboard movement

---
+++ unity_project/Assets/cam_update.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cam_update : MonoBehaviour {
+
+	public float touch_move_scaling = 1.0f;
+	public float keyboard_move_scaling = 2.0f;
+	
+	// Update is called once per frame
+	void Update () {
+	#if UNITY_ANDROID
+		if(Input.touchCount > 0)
+		{
+			Touch touch = Input.touches[0];
+			if (touch.phase == TouchPhase.Moved)
+			{
+				transform.Translate(touch.deltaPosition * Time.deltaTime * -1.0f * touch_move_scaling);
+			}
+		}
+	#else
+		if(Input.GetKey(KeyCode.LeftArrow)) transform.Translate(Vector3.left * Time.deltaTime * keyboard_move_scaling);
+		if(Input.GetKey(KeyCode.RightArrow)) transform.Translate(Vector3.right * Time.deltaTime * keyboard_move_scaling);
+		if(Input.GetKey(KeyCode.UpArrow)) transform.Translate(Vector3.up * Time.deltaTime * keyboard_move_scaling);
+		if(Input.GetKey(KeyCode.DownArrow)) transform.Translate(Vector3.down * Time.deltaTime * keyboard_move_scaling);
+	#endif
+
+	}
+}
---
+++ unity_project/Assets/spawn_floor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn_floor : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject ft;
+
+	void Start () {
+			
+		Quaternion q = Quaternion.LookRotation(new Vector3(0.0f,1.0f,0.0f));
+
+		for(int i=0;i<16;i++) {
+			for (int j = 0;j<16;j++) {
+				bool b = Random.value > 0.3f;
+				if(b)Instantiate(ft, new Vector3(i-8,-0.5f,j-8), q);
+			}
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
