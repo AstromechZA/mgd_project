2d69832931d55ff401c49cab90f944ec9d56b7bf
Ben Meier <benmeier42@gmail.com>
2014-04-17 22:33:09 +0200

    added basic tower test

---
+++ unity_project/Assets/scripts/BasicTowerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTowerControl : MonoBehaviour {
+
+	Component towerTop;
+
+	// Use this for initialization
+	void Start () {
+
+		towerTop = transform.Find("basic_tower_top");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		towerTop.transform.Rotate (Vector3.up * Time.deltaTime * 100.0f);
+	}
+}
