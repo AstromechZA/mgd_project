d8a98130f23ee2f5427e20e686d7a2bb775be65a
Mike36 <michaelferguson6@gmail.com>
2014-04-21 00:57:29 +0200

    Tweaked mobile / tablet formatting

--- unity_project/Assets/scripts/Player.cs
+++ unity_project/Assets/scripts/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour {
 	// Start with no experience
 	public static int experience = 0;
 	public static int castleHealth = 100;
-	
+	void Start() {
+		//Set the screen orientation to landscape
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+}
 	void OnGUI() {	
 		// Draw the players experience and the castles health
 		GUI.Label(new Rect(5, 0, 400, 200), "XP: " + experience);
