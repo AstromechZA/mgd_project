264fdc34b4d39f668d720f2ab996d67561b1a9f7
Ben Meier <benmeier42@gmail.com>
2014-04-23 17:55:57 +0200

    added unity scene

---
+++ unity_project/Assets/objects/guntower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guntower : MonoBehaviour {
+
+	public Transform turretbone;
+	public Transform gunbone;
+
+	private float gunelevation = 0.0f;
+	private float gunelevationPerFrame = 1.0f;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (turretbone != null) turretbone.transform.Rotate (Vector3.left, 1.0f, Space.Self);
+		if (gunbone != null) {
+			gunbone.transform.Rotate (Vector3.up, gunelevationPerFrame, Space.Self);
+			gunelevation += gunelevationPerFrame;
+			if (gunelevation > 200 || gunelevation < -20) gunelevationPerFrame *= -1;
+		}
+	}
+}
