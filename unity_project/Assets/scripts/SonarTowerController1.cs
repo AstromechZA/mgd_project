70e9e75dc9d27ea244cff66110f794971bff603a
Ben Meier <benmeier42@gmail.com>
2014-05-05 11:37:44 +0200

    added controller script for sonar tower

---
+++ unity_project/Assets/scripts/SonarTowerController1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SonarTowerController1 : MonoBehaviour {
+	public float spinrate = 100;
+
+	private Transform topBone;
+
+	void Start () {
+		mapBones ();
+	}
+
+	void Update () {
+		topBone.rotation *= Quaternion.Euler(Time.deltaTime * spinrate, 0, 0);
+	}
+
+	private void mapBones () {
+		Transform a = transform.Find ("Armature");
+		topBone = a.Find("Bone");
+	}
+}
