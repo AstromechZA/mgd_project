7e2371ea245a9c495ffe610bc42a003674f9538f
Ben Meier <benmeier42@gmail.com>
2014-05-06 15:14:39 +0200

    added logic to missile tower

--- unity_project/Assets/scripts/MissileTowerController.cs
+++ unity_project/Assets/scripts/MissileTowerController.cs
@@ -11,6 +11,7 @@ public class MissileTowerController : MonoBehaviour {
 	#endregion
 	#region PRIVATEVARS ===================================================================== //
 
+	private DoorControl doorControl;
 	private const float DOOROPENAMOUNT = 0.3f;
 
 	// bones
@@ -26,12 +27,20 @@ public class MissileTowerController : MonoBehaviour {
 	void Start () {
 		mapBones();
 
-		setDoorProgress(1.0f);
-
+		doorControl = new DoorControl(fireRate);
 	}
 
 	void Update () {
-	
+		Vector3? target = targetMouse();
+
+		if(Input.GetMouseButton(0) && target.HasValue && withinRange(target.Value)) {
+			if(doorControl.isReady()) {
+				doorControl.fire();
+			}
+		}
+		
+		doorControl.update();
+		setDoorProgress(doorControl.getProgress());
 	}
 
 	#endregion
@@ -45,6 +54,20 @@ public class MissileTowerController : MonoBehaviour {
 		eastBone = mainBone.Find("east");
 		westBone = mainBone.Find("west");
 	}
+
+	private Vector3? targetMouse() {
+		Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Plane hp = new Plane(Vector3.up, Vector3.zero);
+		float d = 0;
+		if (hp.Raycast(r, out d)) {
+			return r.GetPoint(d);
+		}
+		return null;
+	}
+
+	private bool withinRange(Vector3 t) {
+		return (transform.position - t).magnitude < this.range;
+	}
 	
 	#endregion
 	#region DOORS ============================================================================ //
@@ -55,6 +78,45 @@ public class MissileTowerController : MonoBehaviour {
 		southBone.transform.localPosition = new Vector3(-1, 0, d);
 		eastBone.transform.localPosition = new Vector3(-1, -d, 0);
 		westBone.transform.localPosition = new Vector3(-1, d, 0);
+	}
+
+	#endregion
+	#region DOORSANIMATOR ==================================================================== //
+
+	private class DoorControl {
+		
+		private float progress = 1.0f;
+		
+		float timeBetweenShots = 1.0f;
+		
+		public DoorControl(float timeBetweenShots) {
+			this.timeBetweenShots = timeBetweenShots;
+		}
+		
+		public float age () {
+			return progress;
+		}
+		
+		public bool isReady () {
+			return progress >= 1.0f;
+		}
+		
+		public void fire () {
+			progress = 0.0f;
+		}
+		
+		public void update () {
+			float inc = Time.deltaTime / timeBetweenShots;
+			progress = progress + inc;
+		}
+		
+		public float ease (float x) {
+			return 1.3f * Mathf.Pow(x, 1/6f) * Mathf.Cos(x * Mathf.PI/2);
+		}
+		
+		public float getProgress () {
+			return ease(Mathf.Clamp01(progress));
+		}
 	}
 
 	#endregion
