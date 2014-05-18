a2459baf90442517b10c2439b0721386a056e7e5
Ben Meier <benmeier42@gmail.com>
2014-05-19 00:12:47 +0200

    added missile trajectory

--- unity_project/Assets/scripts/tower controllers/MissileControl.cs
+++ unity_project/Assets/scripts/tower controllers/MissileControl.cs
@@ -4,24 +4,44 @@ using System.Collections;
 public class MissileControl : MonoBehaviour {
 
 	public float horizantalSpeed = 5f;
+	public float flightDuration = 1.5f; //seconds
 
 	private Vector3 origin;
 	private Vector3 destination;
+
 	private bool inflight = false;
+	private float flightProgress = 0.0f;
 
-	private Vector3 horizantalDir;
-
+	private Vector3 flightVector;
+	private Quaternion angleQuat;
 
 	void Start () {
-	
+		
 	}
 
 	void Update () {
 		if (inflight) {
-			transform.localRotation *= Quaternion.Euler(0, 200 * Time.deltaTime, 0);
-			transform.position += horizantalDir * horizantalSpeed * Time.deltaTime;
+			flightProgress += Time.deltaTime / flightDuration;
 
+			float sflightProgress = squish(flightProgress);
+
+			transform.localRotation = angleQuat * Quaternion.Euler(0,0,-sflightProgress*180);
+
+			Vector3 newpos = origin + sflightProgress * flightVector;
+			newpos.y = 10;
+			transform.position  = newpos;
+
+			float val = (-Mathf.Abs(sflightProgress-0.5f))*6 + 12;
+
+			transform.localScale = val * Vector3.one;
+
+			if (flightProgress > 1) Destroy(this.gameObject);
 		}
+	}
+
+	private float squish(float t) {
+		float tt = (t*2-1);
+		return 0.5f + tt/(1+Mathf.Abs(tt));
 	}
 
 	public void launch(Vector3 target) {
@@ -29,10 +49,16 @@ public class MissileControl : MonoBehaviour {
 		this.destination = target;
 		this.inflight = true;
 
-		Vector3 t = (destination - origin);
-		t.y = 0;
-		horizantalDir = t.normalized;
+		flightVector = (destination - origin);
 
-		transform.position += Vector3.up * 3;
+		angleQuat = Quaternion.Euler(0, -angleTo(this.origin, this.destination), 0);
+	}
+
+	private float angleTo(Vector3 from, Vector3 to) {
+		Vector3 dp = to - from;
+		dp.y = 0;
+		float a = Vector3.Angle(Vector3.right, dp);
+		if (dp.z < 0) {a = -a;}
+		return a;
 	}
 }
