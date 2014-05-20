7587a88fe6ea816b9adb173eba2545536ebe1205
Mike36 <michaelferguson6@gmail.com>
2014-05-20 16:43:53 +0200

    Rename is actually necessary, re added

--- unity_project/Assets/scripts/BuildTower.cs
+++ unity_project/Assets/scripts/BuildTower.cs
@@ -30,6 +30,7 @@ public class BuildTower : MonoBehaviour {
 			enoughCredits = true;
 			
 			Instantiate(gameObject);
+			gameObject.name = "Tower";
 			
 			// Create Placement Visualiser at the same position and rotation as the tower (use y-position that is inbetween game plane and Huds)
 			// Load placement visualisation from Inspector
