c5e166085c82e7da0c5c9c5d65fadfd6cb38007c
Mike36 <michaelferguson6@gmail.com>
2014-05-24 13:06:31 +0200

    Changed skybox and added rotation effect

---
+++ unity_project/Assets/scripts/main menu/RotateCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour {
+
+	public float speed = 2;
+	
+	void Update ()
+	{
+		transform.Rotate(Vector3.up * (speed * Time.deltaTime));
+	}
+}
